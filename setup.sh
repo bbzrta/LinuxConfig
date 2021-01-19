3799e96bdf1ab717fc4159ba3ea3c519de002ee0
Arsham <bbzrta@protonmail.com>
2021-01-19 14:48:26 +0000

    Terminal Config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 folder_structure () {
 	mkdir ~/.fonts ~/.icons ~/.themes ~/temp
 	chmod 777 ~/.fonts ~/.icons ~/.themes
+	clear
 }
 
 # Setting the shell and icon theme in place
@@ -24,13 +25,40 @@ set_themes () {
 
 # Downloading Alacritty on Arch based systems using pacman.
 set_terminal_arch () {
-	sudo pacman -Sy alacritty 
+	sudo pacman -Sy alacritty zsh
+	mv dotconfig/alacritty.yml ~/.config
+	chsh "$USER" -s /bin/zsh
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+	mkdir ~/.Terminal
+	cd ~/.Terminal
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+	echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+	source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 }
 set_terminal_debian (){
-	sudo apt install alacritty
+	sudo apt install alacritty zsh
+	mv dotconfig/alacritty.yml ~/.config
+	chsh "$USER" -s /bin/zsh
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.Terminal/powerlevel10k
+	echo 'source ~/.Terminal/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+	mkdir ~/.Terminal
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.Terminal/zsh-syntax-highlighting
+	echo "source ~/.Terminal/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+	source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 }
 set_terminal_fedora (){
-	sudo dnf install alacritty
+	sudo dnf install alacritty zsh
+	mv dotconfig/alacritty.yml ~/.config
+	chsh "$USER" -s /bin/zsh
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+	mkdir .Terminal
+	cd .Terminal
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+	echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $
+{ZDOTDIR:-$HOME}/.zshrc
+	source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 }
 
 # Installing all the needed packages
@@ -44,60 +72,87 @@ install_packages_fedora(){
 	xargs -a software.txt sudo dnf install
 }
 
-
+install_fonts(){
+	cp ./fonts/* ~/.fonts
+}
 ##########################################################
+clear
 
 echo "1.Arch"
 echo "2.Debian"
 echo "3.Fedora"
-echo "What is the OS based on:"
-read -r osname
+echo "What is the OS based on:"; read -r osname
 clear
+folder_structure
 
-echo "1.Themes"
-echo "2.Terminal"
-echo "3.Software"
-echo "What do you want to start with:"
-read -r task
-clear
+while true ; do
+	clear
 
-if [[ $task -eq 1 ]]
-then
-	set_themes
-fi
+	echo "1.Themes"
+	echo "2.Terminal"
+	echo "3.Software"
+	echo "4.fonts"
+	echo "0.Quit"
+	echo "9.Quit and reboot."
+	echo "What do you want to start with:"
+	read -r task
+	clear
 
-if [[ $task -eq 2 ]]
-then
-	if [[ $osname -eq 1 ]]
+	if [[ $task -eq 1 ]]
 	then
-		set_terminal_arch
-	fi
+		set_themes
+	fi	
 
-	if [[ $osname -eq 2 ]]
+	if [[ $task -eq 2 ]]
 	then
-		set_terminal_debian
-	fi
+		if [[ $osname -eq 1 ]]
+		then
+			set_terminal_arch
+		fi
 
-	if [[ $osname -eq 3 ]]
-	then
-		set_terminal_fedora
-	fi
-fi
+		if [[ $osname -eq 2 ]]
+		then
+			set_terminal_debian
+		fi
 
-if [[ $task -eq 3 ]]
-then
-	if [[ $osname -eq 1 ]]
-	then
-		install_packages_arch
+		if [[ $osname -eq 3 ]]
+		then
+			set_terminal_fedora
+		fi
 	fi
 	
-	if [[ $osname -eq 2 ]]
+	if [[ $task -eq 3 ]]
 	then
-		install_packages_debian
+		if [[ $osname -eq 1 ]]
+		then
+			install_packages_arch
+		fi
+	
+		if [[ $osname -eq 2 ]]
+		then
+			install_packages_debian
+		fi
+
+		if [[ $osname -eq 3 ]]
+		then
+			install_packages_fedora
+		fi
 	fi
 
-	if [[ $osname -eq 3 ]]
+	if [[ task -eq 4 ]]
 	then
-		install_packages_fedora
+		install_fonts
 	fi
-fi
+
+	if [[ $task -eq 0 ]]
+	then
+		break
+	fi
+
+	if [[ $task -eq 9 ]]
+	then
+		chmod 755 ~/.fonts ~/.themes ~/.icons ~/.config
+		rm -rf ~/temp
+		sudo shutdown -r now
+	fi
+done
